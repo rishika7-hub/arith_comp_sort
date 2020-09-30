08f04ef812bea09b680d6aa91ce460609afad40b
rishika7-hub <srishika970@gmail.com>
2020-09-30 16:03:39 +0530

    Added:dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+declare -A dict1
+
 echo "Welcome to Arithmetic Computation and Sorting"
 
 echo "Enter 3 numbers"
@@ -18,3 +21,9 @@ echo $result3
 
 result4=$(( $a % $b + $c ))
 echo $result4
+
+dict1[result1]="$result1"
+dict1[result2]="$result2"
+dict1[result3]="$result3"
+dict1[result4]="$result4"
+
